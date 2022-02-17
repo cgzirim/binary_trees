49b21843960d9975689d328e7c16361cbb68dacf
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2022-02-17 10:53:14 -0800

    Define binary_tree_sibling.

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -13,3 +13,25 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 
 	return (binary_tree_sibling(node->parent));
 }
+
+/**
+* binary_tree_sibling - Finds the sibling of a node.
+* @node: Pointer to the node to find the sibling.
+* Return: If node, parent is NULL or node has no sibling - NULL
+*         Otherwise: Pointer to the node's sibling.
+*/
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (!node->parent->left && !node->parent->right)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
+
+	return (NULL);
+}
