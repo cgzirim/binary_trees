b5bca2b37f86d6e8f4f9033fd7405ea8374cd190
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2022-02-17 10:01:08 -0800

    Return 0 if node is null

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,8 +9,11 @@
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (!node->left && !node->right)
+	if (node)
+	{
+		if (!node->left && !node->right)
 		return (1);
+	}
 
 	return (0);
 }
