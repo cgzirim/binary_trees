fce04484601e7f131e52c4b84a830ed9394afb09
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2022-02-17 10:42:05 -0800

    Add binary_tree_depth definition.

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -30,3 +30,25 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	return (0);
 }
+
+/**
+* binary_tree_depth - Measures the depth of a node in a binary tree.
+* @tree: Pointer to the node to measure the depth.
+*
+* Return: If tree is NULL - 0. Otherwise - depth of the node.
+*/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (tree)
+	{
+		while (tree->parent !=  NULL)
+		{
+			tree = tree->parent;
+			count++;
+		}
+	}
+
+	return (count);
+}
