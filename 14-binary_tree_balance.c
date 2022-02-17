a620f16ad8d669e8314a7b60569e19e91cf52c3f
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2022-02-17 10:28:33 -0800

    Include the `height` function in this file.

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -19,3 +19,22 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (l_subtree - r_subtree);
 }
+
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: Pointer to the root node of the tree to measure the height.
+ * Return: If tree is NULL - 0
+ *         Otherwise - maximum height.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t l = 0, r = 0;
+
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
+	}
+	return (0);
+}
